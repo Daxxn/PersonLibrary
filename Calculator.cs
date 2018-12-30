0ed7a63973f0570953eb081583f8a750df7dffac
Daxxn <46167982+Daxxn@users.noreply.github.com>
2018-12-30 14:07:56 -0800

    Initial commit

---
+++ Calculator.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicLibrary
+{
+    partial class Calculator
+    {
+        public int One { get => 1; }
+        public int Two { get => 2; }
+        public int Three { get => 3; }
+        public int Four { get => 4; }
+        public int Five { get => 5; }
+        public int Six { get => 6; }
+        public int Seven { get => 7; }
+        public int Eight { get => 8; }
+        public int Nine { get => 9; }
+        public int Ten { get => 10; }
+
+        public Calculator() { }
+
+        public double Add(int X, int Y)
+        {
+            return X + Y;
+        }
+    }
+}
---
+++ Person.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicLibrary
+{
+    public class Person
+    {
+        public string FirstName { get; set; }
+        public string Lastname { get; set; }
+
+        public Person() { }
+        public Person(string first, string last)
+        {
+            FirstName = first;
+            Lastname = last;
+        }
+
+        public string FullName
+        {
+            get { return $"{FirstName} {Lastname}"; }
+        }
+    }
+}
